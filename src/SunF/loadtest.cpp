ed1890baa16451869ff4776b3f349d6633ffda56
SUNFLO <sunflower1729@naver.com>
2025-08-20 12:05:06 +0900

    feat: add loadmorse.txt function

--- src/SunF/loadtest.cpp
+++ src/SunF/loadtest.cpp
@@ -3,12 +3,18 @@
 #include <sstream>
 #include <string>
 #include <fstream>
-
 using namespace std;
-unordered_map<char,string> morse_list;
+
+unordered_map<char, string> morse_list;
+
+void printMorseTable() {
+    for (const auto& pair : morse_list) {
+        cout << pair.first << " : " << pair.second << endl;
+    }
+}
 
 void loadMorseTable() {
-    ifstream fin("../include/morse.txt"); 
+    ifstream fin("../include/morse.txt");
     if (!fin.is_open()) {
         cerr << "Error: cannot open morse.txt" << endl;
         return;
@@ -24,22 +30,9 @@ void loadMorseTable() {
     fin.close();
 }
 
-
-string stringToMorse(string s){
-	string ret_string;
-	for(char c: s){
-		ret_string += morse_list[c];
-	}
-
-	return ret_string;
-}
-string morseToString(string) {
-
-}
-saveResult(string input, string morse, string output){
-
-}
-int main(){
-
-	return 0;
+int main() {
+    loadMorseTable();
+    printMorseTable();
+    return 0;
+    
 }
