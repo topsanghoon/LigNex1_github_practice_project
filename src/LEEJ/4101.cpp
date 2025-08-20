7b56c13ad177494e7e48d91d2241924b30fec3da
QQPQ7 <jason898538@gmail.com>
2025-08-20 10:30:02 +0900

    4101LEEJ

---
+++ src/LEEJ/4101.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//#include 
+using namespace std;
+
+int main()
+{
+int a=-1,b=-1;
+while(1)
+{
+cin >> a >>b;
+if(a==0&&b==0) break;
+if (a>b)cout<<"Yes"<<"\n";
+else cout<< "No"<<"\n";
+
+}
+return 0;
+}
+
