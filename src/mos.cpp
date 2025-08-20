1fde47a28021c98f342a2e386acf63b5e5e1cc6e
b.hxxn <chojh000@naver.com>
2025-08-20 12:06:55 +0900

    3func_mos cho

--- src/mos.cpp
+++ src/mos.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <unordered_map>
+#include <fstream>
 #include <sstream>
 using namespace std;
 
+unordered_map<char, string> charToMorse;
+unordered_map<string, char> morseToChar;
 
-loadMorseTable(){
-
-}
-stringToMorse(string s){
-
-}
-string morseToString(string) {
+void loadMorseTable(){
 
 }
-saveResult(string input, string morse, string output){
+string stringToMorse(string s){
+   
+}
+string morseToString(const string& morse) {
+
+    unordered_map<string, char> morseToChar = {
+        {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'},
+        {".", 'E'}, {"..-.", 'F'}, {"--.", 'G'}, {"....", 'H'},
+        {"..", 'I'}, {".---", 'J'}, {"-.-", 'K'}, {".-..", 'L'},
+        {"--", 'M'}, {"-.", 'N'}, {"---", 'O'}, {".--.", 'P'},
+        {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'}
+    };
+
+    stringstream ss(morse);
+    string code;
+    string result;
+    while(ss >> code) {
+        if(code == "/") {
+            result += " ";
+        } else if(morseToChar.find(code) != morseToChar.end()) {
+            result += morseToChar[code];
+        } else {
+            result += '?';
+        }
+    }
+    return result;
+}
+void saveResult(string input, string morse, string output){
 
 }
 int main(){
 
-	return 0;
+
 }
