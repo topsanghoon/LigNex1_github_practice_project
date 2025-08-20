a1b6f6b8ca1e91afdf6e294634b76ab3873fe5de
b.hxxn <chojh000@naver.com>
2025-08-20 12:06:55 +0900

    3func_mos cho

--- src/mos.cpp
+++ src/mos.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <unordered_map>
+#include <fstream>
 #include <sstream>
 #include <string>
 #include <fstream>
 
 using namespace std;
 unordered_map<char,string> morse_list;
+unordered_map<char, string> charToMorse;
+unordered_map<string, char> morseToChar;
 
 void loadMorseTable() {
-    ifstream fin("../include/morse.txt"); 
+    ifstream fin("../include/morse.txt");
     if (!fin.is_open()) {
         cerr << "Error: cannot open morse.txt" << endl;
         return;
@@ -20,26 +23,53 @@ void loadMorseTable() {
     while (fin >> letter >> code) {
         morse_list[letter] = code;
     }
-
     fin.close();
-}
-
-
-string stringToMorse(string s){
-	string ret_string;
-	for(char c: s){
-		ret_string += morse_list[c];
-	}
-
-	return ret_string;
-}
-string morseToString(string) {
 
 }
-saveResult(string input, string morse, string output){
+
+
+string stringToMorse(string s) {
+
+    string ret_string;
+    for (char c : s) {
+        ret_string += charToMorse[c];
+    }
+
+    return ret_string;
 
 }
+
+string morseToString(const string& morse) {
+
+    unordered_map<string, char> morseToChar = {
+        {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'},
+        {".", 'E'}, {"..-.", 'F'}, {"--.", 'G'}, {"....", 'H'},
+        {"..", 'I'}, {".---", 'J'}, {"-.-", 'K'}, {".-..", 'L'},
+        {"--", 'M'}, {"-.", 'N'}, {"---", 'O'}, {".--.", 'P'},
+        {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'}
+    };
+
+    stringstream ss(morse);
+    string code;
+    string result;
+    while(ss >> code) {
+        if(code == "/") {
+            result += " ";
+        } else if(morseToChar.find(code) != morseToChar.end()) {
+            result += morseToChar[code];
+        } else {
+            result += '?';
+        }
+    }
+    return result;
+}
+
+void saveResult(string input, string morse, string output){
+
+}
+
 int main(){
 
-	return 0;
+    return 0;
+
 }
