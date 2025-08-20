f0f5089b60450d4570037fb603f8fed64870a551
SUNFLO <sunflower1729@naver.com>
2025-08-20 15:37:16 +0900

    feat: loadMorseTable 함수 구현

---
+++ src/morse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<char, string> charToMorse;
+unordered_map<string, char> morseToChar;
+
+void loadMorseTable() {
+    ifstream fin("../include/morse.txt"); // src 기준 상대경로
+
+    if (!fin.is_open()) {
+        cerr << "Error: cannot open morse.txt" << endl;
+        return;
+    }
+
+    char letter;
+    string code;
+
+    while (fin >> letter >> code) {
+        charToMorse[letter] = code;
+        morseToChar[code] = letter;
+    }
+
+    fin.close();
+}
+
+
+int main() {
+    loadMorseTable();
+    return 0;
+}
